243e6b8de05c15037b1dff5246ebdb862053145e
Mateus Lisboa <chart010@hotmail.com>
2017-09-26 17:50:32 -0300

    exercicio 02

---
+++ estrutura-sequencial/ex02.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+numero = input("Insira um número: ")
+print(u"O número informado foi {}".format(numero))
