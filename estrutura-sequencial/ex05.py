476aab0d4fd83d554563f7fa346841dcde6c2da2
Mateus Lisboa <chart010@hotmail.com>
2017-10-03 14:45:08 -0300

    enviando exercicio 05

---
+++ estrutura-sequencial/ex05.py
@@ -0,0 +1,3 @@
+#Faça um Programa que converta metros para centímetros.
+metros = float(input("Insira a distancia em metros: "))
+print(u"%0.2f equivale a %0.2f em centímetros"%(metros, metros*100))
