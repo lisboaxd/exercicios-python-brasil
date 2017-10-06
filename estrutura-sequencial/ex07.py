05d8946c0930b5b6c80939441fef6b0360c2eb82
Mateus Lisboa <chart010@hotmail.com>
2017-10-06 14:07:58 -0300

    Calcula area do quadrado e apresenta o dobro

--- estrutura-sequencial/ex07.py
+++ estrutura-sequencial/ex07.py
@@ -1,1 +1,6 @@
-#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+#coding: UTF-8
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro \
+# desta área para o usuário.
+altura = float(input('Digite a Altura do quadrado: '))
+comp = float(input('Digite o Comprimento do quadrado: '))
+print("A área do quadrado é {0} e seu dobro é {1}".format(altura*comp, 2*(altura*comp)))
