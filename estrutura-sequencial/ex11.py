ff7cf39700ec39d014dec32ee0afb54bdca723c1
Mateus Lisboa <chart010@hotmail.com>
2017-10-09 00:47:24 -0300

    Exercicio 1.11

---
+++ estrutura-sequencial/ex11.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que peça 2 números inteiros e um número real. Calcule e mostre:
+	1. o produto do dobro do primeiro com metade do segundo .
+	2. a soma do triplo do primeiro com o terceiro.
+	3. o terceiro elevado ao cubo.
+"""
+num1 = int(input("Insira um número Inteiro: "))
+num2 = int(input("Insira outro número Inteiro: "))
+float3 = float(input("Insira um número Real: "))
+print((num1*2)+(num2/2))
+print((num1*3)+float3)
+print(float3**3)
