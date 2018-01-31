0b11cf31ef5e03f3bcb45a41f993e61fe6d623bb
Mateus Lisboa <chart010@hotmail.com>
2018-01-31 18:21:07 -0200

    Ex02 estrutura decisao

---
+++ estrutura-decisao/ex02.py
@@ -0,0 +1,4 @@
+# coding: UTF-8
+'''Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.'''
+valor = int(input("Digite um valor: "))
+print('Positivo' if valor >= 0 else 'Negativo')
